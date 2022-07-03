76c8ecab6d222362a1d711a951bd7545bd64f73f
7riumph <triumpharamunde@gmail.com>
2022-07-02 23:03:05 -0600

    lint fix

--- spec/services/short_url_service_spec.rb
+++ spec/services/short_url_service_spec.rb
@@ -14,16 +14,10 @@ RSpec.describe ShortUrlService do
 
     it "returns a successful response with correct http request" do
       response = notification_object.create_short_url(original_url)
-
       expect(a_request(:post, "https://api.short.io/links")
-      
         .with(body: {originalURL: original_url, domain: short_io_domain}.to_json, headers: {"Accept" => "application/json", "Content-Type" => "application/json", "Authorization" => "1337"}))
         .to have_been_made.once
-
-
       expect(response.code).to match 200
-
-
       expect(response.body).to match "{\"shortURL\":\"https://42ni.short.gy/jzTwdF\"}"
     end
 
